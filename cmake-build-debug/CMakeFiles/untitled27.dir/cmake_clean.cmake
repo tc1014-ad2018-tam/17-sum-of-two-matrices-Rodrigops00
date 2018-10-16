3962445a19118f8ca344ccee7205325c2c5885f5
Rodrigo <hardy232000@hotmail.com>
2018-10-15 23:52:45 -0500

    commit

---
+++ cmake-build-debug/CMakeFiles/untitled27.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled27.dir/main.c.obj"
+  "untitled27.pdb"
+  "untitled27.exe"
+  "untitled27.exe.manifest"
+  "libuntitled27.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled27.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
